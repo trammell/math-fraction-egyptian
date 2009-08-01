b7b0902496cb3e3527f820034e94bc2b0e00588e
John Trammell <jotr@voh.(none)>
2009-08-01 16:18:16 -0500

    new package for greedy expansion

---
+++ lib/Math/Fraction/Egyptian/greedy.pm
@@ -0,0 +1,47 @@
+
+package Math::Fraction::Egyptian::greedy;
+
+=head2 greedy($n,$d)
+
+Implements Fibonacci's greedy algorithm for computing Egyptian fractions:
+
+    n/d => 1 / ceil(d/n) + ((-d) % n)/(d * ceil(d/n))
+
+1. find the largest integer C<j> such that (1 / j) is not greater than
+C<n/d>.
+
+2. calculate the remainder R: (n/d) - (1/j)
+
+    n/d  =  1/j + R
+
+    R = n/d - 1/j
+      = n/d - 1/ceil(d/n)
+      = nj/dj - d/dj
+      = (nj-d) / dj
+
+      n * ceil(d/n) - d
+
+Example:
+
+    # performing the greedy expansion of 3/7:
+    #   ceil(7/3) = 3
+    #   new numerator = (-7)%3 = 2
+    #   new denominator = 7 * 3 = 21
+    # so 3/7 => 1/3 + 2/21
+
+    my ($n,$d,$e) = greedy(2,7);
+    print "$n/$d ($e)";     # prints "2/21 (3)"
+
+=cut
+
+use POSIX 'ceil';
+
+sub greedy {
+    my ($n,$d) = @_;
+    my $e = ceil( $d / $n );
+    ($n, $d) = simplify(($n - $d) % $n, $d * $e);
+    return ($n, $d, $e);
+}
+
+1;
+
