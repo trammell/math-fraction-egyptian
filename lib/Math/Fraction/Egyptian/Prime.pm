e4356c5f9c1328ba7baeaf58e4c647c779e22e8e
John Trammell <jotr@voh.(none)>
2009-11-06 22:00:20 -0600

    fixing documentation to reflect reality

--- lib/Math/Fraction/Egyptian/Prime.pm
+++ lib/Math/Fraction/Egyptian/Prime.pm
@@ -9,12 +9,21 @@ my %PRIMES = map { $_ => 1 } primes();
 =head1 NAME
 
 Math::Fraction::Egyptian::Prime - construct Egyptian representations of
-fractions with prime denominators
+fractions with odd denominators
 
 =head1 SYNOPSIS
 
     use Math::Fraction::Egyptian::Prime;
     @e = Math::Fraction::Egyptian::Prime->expand(13,23);
+
+=head1 DESCRIPTION
+
+For small odd prime denominators p, the expansion
+
+    2/p = 2/(p + 1) + 2/p(p + 1)
+
+was used.  This identity holds for all positive I<p>, either even or odd.
+For our purposes, only odd values of I<p> are interesting.
 
 =head2 $class->expand($n,$d)
 
