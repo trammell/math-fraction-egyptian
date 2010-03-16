0220f39cc68123d7c388ce00f8a082872fd8b724
John Trammell <johntrammell@gmail.com>
2010-03-16 16:23:19 -0500

    fixed test names

--- lib/Math/Fraction/Egyptian/Practical.pm
+++ lib/Math/Fraction/Egyptian/Practical.pm
@@ -87,49 +87,5 @@ sub summands {
     return @summands;
 }
 
-
-
-
-
-#sub expand {
-#    my ($class,$N,$D) = @_;
-#
-#    # find multiples of $d that are practical numbers
-#    my @mult = grep { is_practical($_ * $D) } 1 .. $D;
-#
-#    warn ">>> @mult";
-#    die "unsuitable strategy" unless @mult;
-#
-#    MULTIPLE:
-#    for my $M (@mult) {
-#        my $n = $N * $M;
-#        my $d = $D * $M;
-#
-#        # find the divisors of $d
-#        my @div = grep { $d % $_ == 0 } 1 .. $d;
-#
-#        # expand $n into a sum of divisors of $d
-#        my @N;
-#        while ($n) {
-#            next MULTIPLE unless @N;
-#            @div = grep { $_ <= $n } @div;
-#            my $x = max @div;
-#            push @N, $x;
-#            $n -= $x;
-#            @div = grep { $_ < $x } @div;
-#        }
-#        my @e = map { $d / $_ } @N;
-#
-#        next MULTIPLE if $e[0] != $M;
-#        next MULTIPLE if grep { $d % $_ } @e[1 .. $#e]; # FIXME
-#
-## o
-##    4. As an observation a1, ..., ai were always divisors of the
-##       denominator a of the first partition 1/a
-#
-#        return (0, 1, @e);
-#    }
-#    die "unsuitable strategy";
-#}
-
 1;
+
--- t/00-load.t
+++
@@ -1,5 +0,0 @@
-use Test::More tests => 1;
-use_ok('Math::Fraction::Egyptian');
-my $v = $Math::Fraction::Egyptian::VERSION;
-diag("\nTesting Math::Fraction::Egyptian, Version $v");
-diag("Perl $], $^X");
